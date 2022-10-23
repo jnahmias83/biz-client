db6dd651e0f4f9e0df3ed5be8a73f4ab1cd9a975
jnahmias83 <jnahmias83@gmail.com>
2022-10-23 15:06:03 +0300

    last version 23.10.22

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ interface FooterProps {
 const Footer: FunctionComponent<FooterProps> = ({ myName }) => {
   return (
     <>
-      <div className="text-light bg-dark p-2">
+      <div className="text-light bg-dark p-2 w-100" style={{zIndex:2,position:"absolute"}}>
         {" "}
         © {myName} 2022 - all right reserved{" "}
       </div>
